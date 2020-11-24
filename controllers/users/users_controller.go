c157693554ac1c018009036008d2b47a01f818ea
Taiki Masuda <t2gucqkc18863j73j2eb@yahoo.co.jp>
2020-11-24 23:34:24 +0900

    dao structure

---
+++ controllers/users/users_controller.go
@@ -0,0 +1,45 @@
+package users
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/taikimasuda/store_users-api/domain/users"
+	"github.com/taikimasuda/store_users-api/services"
+	"github.com/taikimasuda/store_users-api/utils/errors"
+	"net/http"
+	"strconv"
+)
+
+func CreateUser(c *gin.Context) {
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		//user creation error handle
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
+}
+
+func GetUser(c *gin.Context) {
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		//user creation error handle
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/taikimasuda/store_users-api/app"
+
+func main() {
+	app.StartApplication()
+}
+
